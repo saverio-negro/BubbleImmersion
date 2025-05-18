b910b43588411301ba58025ad4b155735b4025be
Saverio Negro <saverionegro@gmail.com>
2025-05-18 22:04:34 +0200

    Initial Commit

---
+++ BubbleImmersion/BubbleImmersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleImmersionApp.swift
+//  BubbleImmersion
+//
+//  Created by Saverio Negro on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BubbleImmersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleImmersion/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BubbleImmersion
+//
+//  Created by Saverio Negro on 18/05/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
