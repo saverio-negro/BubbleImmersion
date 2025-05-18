17e5a1a43cbec5ec1125598d94ad9743e50a632b
Saverio Negro <saverionegro@gmail.com>
2025-05-18 23:06:17 +0200

    Create Immersive View for the Immersive scene. Create `sphereCollection` entity as a container for the grid of sphere to be generated. Lastly, create utility functions for generating the bubbles.

--- BubbleImmersion/Views/ImmersiveView.swift
+++ BubbleImmersion/Views/ImmersiveView.swift
@@ -6,10 +6,42 @@
 //
 
 import SwiftUI
+import RealityKit
+import RealityKitContent
 
 struct ImmersiveView: View {
+    
+    // Entity containing all sphere model entities
+    let sphereCollection = Entity()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RealityView { content in
+            
+        }
+    }
+    
+    func getRandomColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        let alpha = CGFloat.random(in: 0.5...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    func generateRandomSphere() -> ModelEntity {
+        let randomRadius = Float.random(in: 0.05...0.1)
+        let mesh = MeshResource.generateSphere(radius: randomRadius)
+        let material = SimpleMaterial(
+            color: getRandomColor(),
+            roughness: .float(Float.random(in: 0...1)),
+            isMetallic: Bool.random()
+        )
+        let sphere = ModelEntity(mesh: mesh, materials: [material])
+        sphere.generateCollisionShapes(recursive: true)
+        sphere.components.set(HoverEffectComponent())
+        sphere.components.set(InputTargetComponent(allowedInputTypes: [.direct, .indirect]))
+        return sphere
     }
 }
 
